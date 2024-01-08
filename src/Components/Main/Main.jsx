4dc6a1ee4a4022d9b30fcd73136fec6ac6044e54
tjduddn8511 <130065523+tjduddn8511@users.noreply.github.com>
2024-01-08 17:36:44 +0900

    bbeep.side

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,50 +1,48 @@
 import React, { useState } from "react";
 import "./Main.css";
+import Side from "../Side/Side";
+import { useNavigate } from "react-router-dom";
+const Bbeep = () => {
+  const navigate = useNavigate();
+  let toggle;
+  const HandleSearchRoom = () => {
+    let Room = document.getElementById("RoomCheck");
+    let Class = document.getElementById("ClassCheck");
 
-    const Bbeep = () => {
-    let toggle;
-    const HandleSearchRoom = () => {
-        let Room = document.getElementById("roomcheck");
-        let Class = document.getElementById("classcheck");
+    if (!toggle) {
+      Room.style.color = "blue";
+      Class.style.color = "black";
+      toggle = true;
+    } else {
+      Class.style.color = "blue";
+      Room.style.color = "black";
+      toggle = false;
+    }
+  };
 
+  const HandleTouchGrade = () => {
+    let Hi;
+  };
 
-        if (!toggle) {
-            Room.style.color = "blue";
-            Class.style.color = "black";
-            toggle = true;
-        } else {
-            Class.style.color = "blue";
-            Room.style.color = "black";
-            toggle = false;
-        }
-    };
-
-    const HandleTouchGrade = () => {
-        let Hi;
-    };
-
-    return (
-        <div>
-            <div className="header-bar">
-        <span id="roomcheck" onClick={HandleSearchRoom} className="first">
+  return (
+    <div className="MainWrapper">
+      <Side />
+      <div className="HeaderBar">
+        <span id="RoomCheck" onClick={HandleSearchRoom} className="first">
           실 조회하기
         </span>
-                <span id="classcheck" onClick={HandleSearchRoom}>
+        <span id="ClassCheck" onClick={HandleSearchRoom}>
           학반 조회하기
         </span>
-            </div>
-
-
-
-            <div className="main">
-                <div>1반</div>
-                <div>2반</div>
-                <div>3반</div>
-                <div>4반</div>
-            </div>
-        </div>
-    );
-    };
+      </div>
+      <div className="Main">
+        <div>1반</div>
+        <div>2반</div>
+        <div>3반</div>
+        <div>4반</div>
+      </div>
+    </div>
+  );
+};
 
 export default Bbeep;
-
--- src/Components/side/side.jsx
+++ src/Components/side/side.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
+import "../../style/Side.style/Side.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 const Side = () => {
-    return(
+  return (
+    <div>
+      <div className="SideBarWrap">
+        <h1>삑</h1>
         <div>
-            <div className="side-bar">
-                <div className="one" >
-                    1학년
-                </div>
-                <div className="two" >
-                    2학년
-                </div>
-                <div className="three">
-                    3학년
-                </div>
-            </div>
+          <FontAwesomeIcon icon={faGraduationCap} color="red" width={"1vw"} height={"1vh"} />
+          <div className="Grade1">1학년</div>
         </div>
-    );
+
+        <div className="Grade2">2학년</div>
+        <div className="Grade3">3학년</div>
+      </div>
+    </div>
+  );
 };
 
 export default Side;
