f8209bd2ac446cc82c869763a9fbc11739f8b9ae
jeonminchan <chan2bo2@dgsw.hs.kr>
2024-01-10 20:17:16 +0900

    feat :: ShowPw

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,8 @@
 import "../../style/Login.style/Login.css";
 import Logo from "../../img/Logo.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { showToast } from "../../lib/Swal/Swal";
+import { useNavigate } from "react-router-dom";
 
 const User = {
   id: "yeonggu1234",
@@ -13,8 +14,13 @@ const Login = () => {
   const [pwValid, setPwValid] = useState(false);
   const [notAllow, setNotAllow] = useState(true);
 
+  const [isShowPassWord, setIsShowPassWord] = useState(false);
+  const ShowPassWordRef = useRef(null);
+
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (idValid && pwValid) {
@@ -44,17 +50,28 @@ const Login = () => {
     }
   };
 
+  const handleShowPwChecked = async () => {
+    const password = await ShowPassWordRef.current;
+    if (password === null) return;
+
+    await setIsShowPassWord(!isShowPassWord);
+    if (!isShowPassWord) {
+      password.type = "text";
+    } else {
+      password.type = "password";
+    }
+  };
+
   const onClickConfirmButton = () => {
-    const ID = document.getElementById("id");
-    const PW = document.getElementById("pw");
     if (id === User.id && pw === User.pw) {
       showToast("success", "Login");
+      navigate("/main");
     } else if (id !== User.id && pw === User.pw) {
       showToast("error", "ID");
-      ID.value = null;
+      setId("");
     } else {
       showToast("error", "PW");
-      PW.value = null;
+      setPw("");
     }
   };
 
@@ -64,10 +81,23 @@ const Login = () => {
         <img src={Logo} alt="" className="Logo" />
         <div className="InputWrap">
           <h1>Log in</h1>
-          <input className="ID" placeholder="ID" id="id" value={id} onChange={handleID} />
+          <input className="ID" placeholder="ID" value={id} onChange={handleID} />
           {!idValid && <div className="errorMessageWrap">ID를 입력해주세요.</div>}
-          <input type="password" className="PW" placeholder="Password" id="pw" value={pw} onChange={handlePw} />
+          <input
+            type="password"
+            className="PW"
+            placeholder="Password"
+            value={pw}
+            onChange={handlePw}
+            ref={ShowPassWordRef}
+          />
           {!pwValid && <div className="errorMessageWrap">비밀번호를 입력해주세요.</div>}
+          <div className="ShowPwBtn">
+            <label>
+              <input type="checkbox" onChange={handleShowPwChecked} />
+              <span>비밀번호 보X</span>
+            </label>
+          </div>
           <div className="ButtonWrap">
             <button className="LoginBtn" disabled={notAllow} onClick={onClickConfirmButton}>
               Log in
