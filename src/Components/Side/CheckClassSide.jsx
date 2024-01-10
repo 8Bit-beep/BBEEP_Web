7861b5c44a34fa42b4ce735f0f967d4d5a4cd97b
jeonminchan <chan2bo2@dgsw.hs.kr>
2024-01-10 09:41:56 +0900

    feat :: CheckClassSide

---
+++ src/Components/Side/CheckClassSide.jsx
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import "../../style/Side.style/Side.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCode, faSchool,  } from "@fortawesome/free-solid-svg-icons";
+import { useLocation, useNavigate } from "react-router-dom";
+const CheckClassSide = () => {
+  const [clickedCategory, setIsClickedCategory] = useState(null);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/lab-class":
+        setIsClickedCategory("실습동");
+        break;
+      case "/main-class":
+        setIsClickedCategory("본관 1층");
+        break;
+      case "/exercise-class":
+        setIsClickedCategory("강당");
+        break;
+      case "/special-class":
+        setIsClickedCategory("별관");
+      default:
+        setIsClickedCategory(null);
+    }
+  }, [location.pathname]);
+
+  const onClickGradeCategory = (grade) => {
+    setIsClickedCategory((prevGrade) => (prevGrade === grade ? null : grade));
+
+    switch (grade) {
+      case "실습동":
+        navigate("/lab-class");
+        break;
+      case "본관 1층":
+        navigate("/main-class");
+        break;
+      case "강당":
+        navigate("/exercise-class");
+        break;
+      case "별관":
+        navigate("/special-class");
+      default:
+        navigate(null);
+    }
+  };
+
+  const onClickNavigateMain = () => {
+    navigate("/main");
+  };
+
+  return (
+    <div>
+      <div className="SideBarWrap">
+        <h1 onClick={onClickNavigateMain}>삑</h1>
+        <div
+          className={clickedCategory === "실습동" ? "Selected1" : "Class"}
+          onClick={() => onClickGradeCategory("실습동")}
+        >
+          <FontAwesomeIcon icon={faCode} color="white" className="icon" />
+          <span>실습동</span>
+        </div>
+        <div
+          className={clickedCategory === "본관 1층" ? "Selected2" : "Class"}
+          onClick={() => onClickGradeCategory("본관 1층")}
+        >
+          <FontAwesomeIcon icon={faSchool} color="white" className="icon" />
+          <span>본관 1층</span>
+        </div>
+        <div
+          className={clickedCategory === "강당" ? "Selected3" : "Class"}
+          onClick={() => onClickGradeCategory("강당")}
+        >
+          <FontAwesomeIcon icon={faGraduationCap} color="white" className="icon" />
+          <span>강당</span>
+        </div>
+        <div
+          className={clickedCategory === "별관" ? "Selected3" : "Class"}
+          onClick={() => onClickGradeCategory("별관")}
+        >
+          <FontAwesomeIcon icon={faGraduationCap} color="white" className="icon" />
+          <span>별관</span>
+        </div>
+        <button className="ShowChatBtn">메시지 보기</button>
+      </div>
+    </div>
+  );
+};
+
+export default CheckClassSide;
