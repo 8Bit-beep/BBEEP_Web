e20c8b451847add3503337d6011f652467b0e76b
tjduddn8511 <130065523+tjduddn8511@users.noreply.github.com>
2024-02-01 23:20:33 +0900

    design

---
+++ src/Components/Massege/Massege.jsx
@@ -0,0 +1,12 @@
+import {React } from "react";
+import Side from "../Side/Side"
+const Massege = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+
+export default Massege;
