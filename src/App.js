08c04f62afab3045be2b6423fdfa103b7bbe4c39
jeonminchan <chan2bo2@dgsw.hs.kr>
2024-02-15 00:11:14 +0900

    Revert "feat :: Header #2"
    This reverts commit b520c49c752b3408b8b45813f4903b3b40f50d1e.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import CheckClass from "./components/CheckClass/CheckClass";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/checkClass" element={<CheckClass />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+
+    </div>
   );
 }
 
